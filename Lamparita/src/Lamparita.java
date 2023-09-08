273258af00d2007e2e249fa7b3f79cf93d7d19ad
Cami <camistrika@gmail.com>
2023-09-08 17:58:38 -0300

    cambios hechos

--- Lamparita/src/Lamparita.java
+++ Lamparita/src/Lamparita.java
@@ -1,4 +1,21 @@
 public class Lamparita {
+    private boolean estado;
+    public lamparita(){
+        this.estado=false
+    }
+
+    public void encender(){
+        this.estado=true;
+    }
+
+    public void apagar(){
+        this.estado=false;
+    }
+
+    public boolean estado(){
+        return estado;
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Vamos a programar una lamparita! ");
     }
